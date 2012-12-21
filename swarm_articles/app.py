c0f8052e20141ef80ba4fe1573d6404d12eac1e5
Denis Mishchishin <dennz78@gmail.com>
2012-12-21 07:16:08 +0200

    crawl draft

--- swarm_articles/app.py
+++ swarm_articles/app.py
@@ -1,18 +1,97 @@
-import logging
 
+import logging
+from pprint import pprint
+from urlparse import urlparse
+import re
+
+from werkzeug.datastructures import ImmutableDict
 
 from swarm import transport, swarm, define_swarm
 from swarm.ext.http import HtmlSwarm
+from swarm.ext.http.helpers import parser
 from swarm.ext.articles.text import PageText
- 
+
+class ArticlesSwarm(HtmlSwarm):
+    default_config = ImmutableDict(HtmlSwarm.default_config,
+                     **{'NON_DOCUMENT_CONTENT':['.gif',
+                                                '.jpeg',
+                                                '.jpg',
+                                                '.css',
+                                                '.js',
+                                                '.png',
+                                                '.ico',
+                                                '.xml'],
+                    })
+
 define_swarm.start()
 
-articles = HtmlSwarm(__name__)
+articles = ArticlesSwarm(__name__)
 
-@articles.url('/crawl/<path:url>')
-def crawl(url, greed=1):
-    with swarm(url) << 'greed':
+@parser
+def document_urls(html):
+    if html is None:
+        return
+    html.make_links_absolute(transport.url)
+    for element, attribute, link, pos in html.iterlinks():
+        if any((link.endswith(NON_DOCUMENT) for NON_DOCUMENT in swarm.object.config['NON_DOCUMENT_CONTENT'])):
+            continue
+        yield link
+
+
+def filter_domains(urls, domains=[]):
+    for url in urls:
+        parsed = urlparse(url)
+        for domain in domains:
+            if domain.startswith('.') and parsed.hostname.endswith(domain[1:]):
+                yield url
+                break
+            elif parsed.hostname == domain:
+                yield url
+                break
+
+def filter_by_regexps(urls, regexps=[], allow=True):
+    for url in urls:
+        matches = any((re.match(regexp, url) for regexp in regexps))
+        if allow and matches:
+            yield url
+        elif not allow and not matches:
+            yield url
+
+
+
+
+def crawl(  urls=[],
+            greed=1,
+            follow=True,
+            allow_links=[],
+            deny_links=[],
+            limit_domains=[],):
+
+    with swarm(*urls) << 'greed, limit_domains, follow, allow_links, deny_links':
         yield PageText(transport.content,
-                           url=transport.url).winner(greed=greed)
+                       url=transport.url).winner(greed=greed)
+        if not follow:
+            return
+        urls = document_urls()
+        if limit_domains:
+            urls = filter_domains(urls, limit_domains)
+
+        if allow_links:
+            urls = filter_by_regexps(urls, allow_links)
+        if deny_links:
+            urls = filter_by_regexps(urls, deny_links, allow=False)
+
+        urls = list(set(urls))
+        crawl(urls=urls,
+              greed=greed,
+              limit_domains=limit_domains,
+              follow=follow,
+              allow_links=allow_links,
+              deny_links=deny_links)
+
+@articles.url('/start')
+def start(datasource=None, **kwargs):    
+    if datasource is None:
+        crawl(**kwargs)
 
 define_swarm.finish()
---
+++ swarm_articles/main.py
@@ -0,0 +1,117 @@
+import logging
+import sys
+import os
+from argparse import Action
+
+from cliff.app import App
+from cliff.commandmanager import CommandManager
+
+from werkzeug.utils import import_string
+
+DEFAULT_INSTANCE_PATH = './.swarm'
+
+OPT_MODULES = {
+    'cookies':'swarm.ext.http.signal.cookies',
+    'cache':'swarm.ext.http.signal.cache',
+}
+
+
+
+class store_int(Action):
+    def __call__(self, parser, args, value, option_string=None):
+        setattr(args, self.dest, int(value))
+
+class ArticlesApp(App):
+    log = logging.getLogger(__name__)
+    def configure_logging(self):
+        pass
+
+    def __init__(self):
+        super(ArticlesApp, self).__init__(
+            description='articles crawler',
+            version='0.0.1',
+            command_manager=CommandManager('swarm_articles.commands'),
+            )
+
+        self._clean_files = []
+
+    def initialize_app(self, argv):
+        from .app import articles
+        #get file config is configured
+
+        if self.options.config:
+            articles.config.from_file(self.options.config)
+
+        if self.options.workers:
+            articles.competitors = articles.config['COMPETITORS'] = self.options.workers
+
+        #setup logging
+        from breadability.logconfig import LOG
+
+        articles.config['DEBUG'] = articles.debug = self.options.debug
+
+        #configure signalsets fron args
+        if articles.reloaded_module is None:
+            articles.reloaded_module = ['swarm.ext.http.signal.unique_url',]
+            # articles.reloaded_module = []
+
+        for name, mod in OPT_MODULES.items():
+            if getattr(self.options, name):
+                articles.reloaded_module.append(import_string(mod))
+
+        #setup instance papth
+        articles.instance_path = self.options.instance_path
+
+        self.articles = articles
+
+    def clean_up(self, cmd, result, err):
+        self.clean_files()
+
+    def clean_files(self):
+        for path in self._clean_files:
+            if os.path.exists(path) and os.path.isfile(path):
+                os.unlink(path)
+
+
+    def build_option_parser(self, *args, **kwargs):
+        parser = super(ArticlesApp, self).build_option_parser(*args, **kwargs)
+        parser.add_argument(
+            '--instance',
+            dest='instance_path',
+            metavar='INSTANCE',
+            action='store',
+            default=DEFAULT_INSTANCE_PATH,
+            help='Specify an instance folder (default: "%s")'%DEFAULT_INSTANCE_PATH,
+            )
+
+        parser.add_argument(
+            '--workers',
+            dest='workers',
+            metavar='INTEGER',
+            action=store_int,
+            default=None,
+            help='Set number of workers',
+            )
+
+
+        parser.add_argument(
+            '--config',
+            default=None,
+            action='store',
+            help='Specify config file',
+            )
+
+        for name in OPT_MODULES:
+            parser.add_argument(
+                '--%s'%name,
+                default=False,
+                action='store_true',
+                help='Enable %s'%name,
+                )
+
+
+        return parser
+
+def main(argv=sys.argv[1:]):
+    articles = ArticlesApp()
+    return articles.run(argv)
